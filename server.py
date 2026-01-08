8f96e29e8c77238e0681c7106c3e232a8c50784e
Qwamatata <malyutin.2011@list.ru>
2026-01-08 19:43:01 +0300

    Commands added. /Users added

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import socket
 import threading
 import IlyasMessageProtocol
@@ -10,6 +11,7 @@ full_message = b'start chat\n'
 
 def client_handler(client_socket):
     global full_message
+    global connected_clients
     attempts = 3
     while attempts != 0:
         name = IlyasMessageProtocol.receive(client_socket)[1]
@@ -33,39 +35,49 @@ def client_handler(client_socket):
             IlyasMessageProtocol.send(client_socket, '200'.encode(), 'ERR')
             client_socket.close()
             break
-        if string.decode()[0] == '@':
-            names_and_message = string.decode().split(' ')
-            direct_names = []
-            message_list = []
-            message = b''
-
-            for index in range(len(names_and_message)):
-                if names_and_message[index][0] == '@':
-                    direct_names.append(names_and_message[index].split('@')[1])
-                else:
-                    message_list.append(names_and_message[index])
-            print(direct_names)
-            receivers = ','.join(direct_names)
-            message = ' '.join(message_list).encode()
-            for direct_name in direct_names:
-
-                if direct_name in connected_clients:
-                    IlyasMessageProtocol.send(connected_clients[direct_name],
-                                              ' '.join([name, message.decode()]).encode(), 'TXT', 'a')
-                else:
-                    IlyasMessageProtocol.send(client_socket, f'User @{direct_name} offline'.encode(), 'TXT', 'a')
+        if string.decode()[0] == '/':
+            if string.decode() == '/users':
+                all_users = db_client.execute_query('SELECT username FROM Users', '62.60.178.229', 10052)
+                for user in all_users:
+                    if user in connected_clients:
+                        user['status'] = 'online'
+                    else:
+                        user['status'] = 'offline'
+                IlyasMessageProtocol.send(client_socket, json.dumps(all_users).encode(), 'JSN')
         else:
-            message = string
-            for client_name in connected_clients:
-                if client_name != name:
-                    IlyasMessageProtocol.send(connected_clients[client_name], f'{name}:{string.decode()}'.encode(),
-                                              'TXT', 'a')
-            receivers = 'no users'
-        current_time = datetime.datetime.now()
-        date = current_time.date().strftime('%d.%m.%y')
-        time = current_time.time().strftime('%H:%M:%S')
+            if string.decode()[0] == '@':
+                names_and_message = string.decode().split(' ')
+                direct_names = []
+                message_list = []
+                message = b''
 
-        print(db_client.execute_query(f'INSERT INTO messages (text, author, date, time, receivers) VALUES ({message.decode()}, {name}, {date}, {time}, {receivers});', '127.0.0.1', 10052))
+                for index in range(len(names_and_message)):
+                    if names_and_message[index][0] == '@':
+                        direct_names.append(names_and_message[index].split('@')[1])
+                    else:
+                        message_list.append(names_and_message[index])
+                print(direct_names)
+                receivers = ','.join(direct_names)
+                message = ' '.join(message_list).encode()
+                for direct_name in direct_names:
+
+                    if direct_name in connected_clients:
+                        IlyasMessageProtocol.send(connected_clients[direct_name],
+                                                  ' '.join([name, message.decode()]).encode(), 'TXT', 'a')
+                    else:
+                        IlyasMessageProtocol.send(client_socket, f'User @{direct_name} offline'.encode(), 'TXT', 'a')
+            else:
+                message = string
+                for client_name in connected_clients:
+                    if client_name != name:
+                        IlyasMessageProtocol.send(connected_clients[client_name], f'{name}:{string.decode()}'.encode(),
+                                                  'TXT', 'a')
+                receivers = 'no users'
+            current_time = datetime.datetime.now()
+            date = current_time.date().strftime('%d.%m.%y')
+            time = current_time.time().strftime('%H:%M:%S')
+
+            print(db_client.execute_query(f'INSERT INTO messages (text, author, date, time, receivers) VALUES ({message.decode()}, {name}, {date}, {time}, {receivers});', '62.60.178.229', 10052))
 
     client_socket.close()
 
